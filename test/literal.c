29e0b5db117c5920355a9fcf7e22b441cf0842b1
Edward Baudrez <edward.baudrez@gmail.com>
2014-03-28 14:19:30 +0100

    add a test to literal.c

--- test/literal.c
+++ test/literal.c
@@ -3,18 +3,38 @@
 #include "pathcomp.h"
 
 const char *config = "\
-[test.literal]\n\
-    key = value\
+[test.literal1]\n\
+    key = value\n\
+\n\
+[test.literal2]\n\
+    instrument = G2\n\
+    imager     = SEV1\n\
+    level      = 20\n\
+    resolution = HR\n\
+    product    = SOL_TH\n\
 ";
 
 static void
-test_literal( const char *class_name )
+test_literal1()
 {
     pathcomp_t *composer = NULL;
-    ok( composer = pathcomp_new(class_name) );
+    ok( composer = pathcomp_new("test.literal1") );
     const char *val = pathcomp_eval( composer, "key" );
     is( val, "value" );
     pathcomp_free( composer );
+}
+
+static void
+test_literal2()
+{
+    pathcomp_t *c = NULL;
+    ok( c = pathcomp_new("test.literal2") );
+    is( pathcomp_eval(c, "instrument"), "G2" );
+    is( pathcomp_eval(c, "imager"), "SEV1" );
+    is( pathcomp_eval(c, "level"), "20" ); /* everything evaluates as strings */
+    is( pathcomp_eval(c, "resolution"), "HR" );
+    is( pathcomp_eval(c, "product"), "SOL_TH" );
+    pathcomp_free( c );
 }
 
 int
@@ -22,7 +42,8 @@ main( void )
 {
     plan( NO_PLAN );
     pathcomp_use_config_from( config );
-    test_literal( "test.literal" );
+    test_literal1();
+    test_literal2();
     pathcomp_cleanup();
     done_testing();
 }
