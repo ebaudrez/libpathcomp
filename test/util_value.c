4d897d55b79927afdf897c76480f8b1fd156226b
Edward Baudrez <edward.baudrez@gmail.com>
2014-05-23 17:19:42 +0200

    test val->type as well

--- test/util_value.c
+++ test/util_value.c
@@ -10,6 +10,7 @@ test_literal(void)
     value_t *val;
 
     ok(val = value_new("abc"));
+    cmp_ok(val->type, "==", VALUE_LITERAL);
     is(value_eval(val, NULL, NULL), "abc");
     value_free(val);
 }
@@ -20,12 +21,15 @@ test_lua(void)
     value_t *val;
 
     ok(val = value_new("lua { return 1+2 }"));
+    cmp_ok(val->type, "==", VALUE_LUA);
     is(value_eval(val, NULL, NULL), "3");
     value_free(val);
     ok(val = value_new("lua { return some_weird_name + 2 }"));
+    cmp_ok(val->type, "==", VALUE_LUA);
     ok(!value_eval(val, NULL, NULL), "use of undefined variables raises an error");
     value_free(val);
     ok(val = value_new("lua { return self.slot }"));
+    cmp_ok(val->type, "==", VALUE_LUA);
     ok(!value_eval(val, NULL, NULL), "inadvertent use of value_eval() with NULL composer raises an error");
     value_free(val);
     /* can't test value_lua_t more thoroughly without a composer object and a
