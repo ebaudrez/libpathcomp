756389f48f3e72f194ddf3638ddd4ea1eabfa669
Edward Baudrez <edward.baudrez@gmail.com>
2014-10-15 18:56:07 +0200

    improve the error reporting of run_valgrind.sh

--- test/run_valgrind.sh
+++ test/run_valgrind.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+
 files="$*"
 test -z "$files" && files=.libs/test_*
 > valgrind.log
 for f in $files
 do
-    echo "--$f"
-    LD_LIBRARY_PATH=../src/.libs/ valgrind --leak-check=full --show-reachable=yes $f 2>&1 | tee -a valgrind.log | grep 'no leaks are possible'
+    echo -n "--$f"
+    if ! test -x "$f"; then
+        echo " (not executable)"
+        continue
+    else
+        echo
+    fi
+    output=`LD_LIBRARY_PATH=../src/.libs/ valgrind --leak-check=full --show-reachable=yes "$f" 2>&1 | tee -a valgrind.log | grep 'no leaks are possible'`
+    if test "$output"; then
+        echo "$output"
+    else
+        echo -e '    ** \e[0;31;1mWarning!\e[0m    No output!'
+    fi
 done
