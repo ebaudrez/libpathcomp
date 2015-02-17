d93f2aa29516364a343aa64c18f7602ea4a92f95
Edward Baudrez <edward.baudrez@gmail.com>
2015-02-17 16:11:43 +0100

    transfer Doxygen comments from pathcomp.c to the header file

--- include/pathcomp.h
+++ include/pathcomp.h
@@ -20,14 +20,66 @@ extern void        pathcomp_add_config_from_file(const char *);
 extern void        pathcomp_cleanup(void);
 extern pathcomp_t *pathcomp_new(const char *);
 extern void        pathcomp_free(pathcomp_t *);
+
+/**
+ * \note The string returned by this function must not be deallocated by the user.
+ * \note Beware: this function may return a pointer to internal storage. There
+ * are no guarantees on the lifetime of the object pointed to by the return
+ * value of this function. Therefore, this function should probably only be
+ * used for debugging. Prefer pathcomp_eval().
+ * \seealso pathcomp_eval()
+ */
 extern const char *pathcomp_eval_nocopy(pathcomp_t *, const char *);
+
+/**
+ * \note The string returned by this function must be deallocated by the user.
+ */
 extern char       *pathcomp_eval(pathcomp_t *, const char *);
+
+/**
+ * \note The string returned by this function must be deallocated by the user.
+ */
 extern char       *pathcomp_yield(pathcomp_t *);
+
+/**
+ * pathcomp_set() will reset all alternatives to their first value. This is
+ * necessary because alternatives are tried in order, and there is no guarantee
+ * that a matching combination of alternatives comes later in the list of
+ * possible combinations.
+ *
+ * Imagine the situation where you have successfully located a GERB archive
+ * file with the following characteristics:
+ *
+ *    -# instrument = G2
+ *    -# extension  = .hdf
+ *    -# extension  = .hdf.gz
+ *    -# etc.
+ *
+ * After executing pathcomp_find(), the \e extension alternative will point to
+ * the .hdf.gz value, because G2 files are gzip-compressed. If you now call
+ *
+ *   pathcomp_set(composer, "instrument", "GL")
+ *
+ * pathcomp_find() will fail, because GERB-like files are not compressed
+ * externally, and the alternative .hdf comes before .hdf.gz in the list of
+ * alternatives, and pathcomp_find() will not rewind it! Therefore,
+ * pathcomp_reset() must be called to rewind all the alternatives.
+ */
 extern void        pathcomp_set(pathcomp_t *, const char *, const char *);
+
+/**
+ * All alternatives are rewound after calling this function, so that a failed
+ * search with pathcomp_find() can be retried using the newly added
+ * alternative.
+ */
 extern void        pathcomp_add(pathcomp_t *, const char *, const char *);
 extern void        pathcomp_reset(pathcomp_t *);
 extern int         pathcomp_done(pathcomp_t *);
 extern int         pathcomp_next(pathcomp_t *);
+
+/**
+ * \note The string returned by this function must be deallocated by the user.
+ */
 extern char       *pathcomp_find(pathcomp_t *);
 extern int         pathcomp_mkdir(pathcomp_t *);
 
--- src/pathcomp.c
+++ src/pathcomp.c
@@ -146,14 +146,6 @@ pathcomp_free(pathcomp_t *composer)
     free(composer);
 }
 
-/**
- * \note The string returned by this function must not be deallocated by the user.
- * \note Beware: this function may return a pointer to internal storage. There
- * are no guarantees on the lifetime of the object pointed to by the return
- * value of this function. Therefore, this function should probably only be
- * used for debugging. Prefer pathcomp_eval().
- * \seealso pathcomp_eval()
- */
 const char *
 pathcomp_eval_nocopy(pathcomp_t *composer, const char *name)
 {
@@ -166,18 +158,12 @@ pathcomp_eval_nocopy(pathcomp_t *composer, const char *name)
     return att_eval(att, composer, composer->metatable);
 }
 
-/**
- * \note The string returned by this function must be deallocated by the user.
- */
 char *
 pathcomp_eval(pathcomp_t *composer, const char *name)
 {
     return strdup(pathcomp_eval_nocopy(composer, name));
 }
 
-/**
- * \note The string returned by this function must be deallocated by the user.
- */
 char *
 pathcomp_yield(pathcomp_t *composer)
 {
@@ -199,30 +185,6 @@ pathcomp_yield(pathcomp_t *composer)
     return NULL;
 }
 
-/**
- * pathcomp_set() will reset all alternatives to their first value. This is
- * necessary because alternatives are tried in order, and there is no guarantee
- * that a matching combination of alternatives comes later in the list of
- * possible combinations.
- *
- * Imagine the situation where you have successfully located a GERB archive
- * file with the following characteristics:
- *
- *    -# instrument = G2
- *    -# extension  = .hdf
- *    -# extension  = .hdf.gz
- *    -# etc.
- *
- * After executing pathcomp_find(), the \e extension alternative will point to
- * the .hdf.gz value, because G2 files are gzip-compressed. If you now call
- *
- *   pathcomp_set(composer, "instrument", "GL")
- *
- * pathcomp_find() will fail, because GERB-like files are not compressed
- * externally, and the alternative .hdf comes before .hdf.gz in the list of
- * alternatives, and pathcomp_find() will not rewind it! Therefore,
- * pathcomp_reset() must be called to rewind all the alternatives.
- */
 void
 pathcomp_set(pathcomp_t *composer, const char *name, const char *value)
 {
@@ -233,11 +195,6 @@ pathcomp_set(pathcomp_t *composer, const char *name, const char *value)
     pathcomp_reset(composer);
 }
 
-/**
- * All alternatives are rewound after calling this function, so that a failed
- * search with pathcomp_find() can be retried using the newly added
- * alternative.
- */
 void
 pathcomp_add(pathcomp_t *composer, const char *name, const char *value)
 {
@@ -288,9 +245,6 @@ path_exists(const char *path)
     return stat(path, &statbuf) == 0;
 }
 
-/**
- * \note The string returned by this function must be deallocated by the user.
- */
 char *
 pathcomp_find(pathcomp_t *composer)
 {
