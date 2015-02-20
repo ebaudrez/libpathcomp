09b185a31005afad981ad1e9406d2680b105a1aa
Edward Baudrez <edward.baudrez@gmail.com>
2015-02-20 08:46:00 +0100

    add more documentation for pathcomp/log.h

--- include/pathcomp/log.h
+++ include/pathcomp/log.h
@@ -4,8 +4,11 @@
  *
  * The \a libpathcomp library uses these routines to log its debug, warning,
  * and error messages. Default implementations are provided, but these may be
- * overridden by the user. This header file should be included to provide the
- * proper declarations.
+ * overridden by the user. The default implementations log to \a stderr.
+ *
+ * If you intend to override these functions, this header file should be
+ * included to provide the proper declarations. If the default implementations
+ * of these functions are acceptable, there is no need to include this header.
  */
 
 #ifndef PATHCOMP_LOG_INCLUDED
@@ -18,18 +21,32 @@ extern "C" {
 /**
  * \brief Log a message at debug level
  * \param fmt <em>sprintf()</em>-style format string
+ * \param ... Additional arguments according to format string
+ *
+ * The default implementation of this function silently discards debug
+ * messages.
  */
 extern void pathcomp_log_debug(const char *fmt, ...);
 
 /**
  * Log a message at warning level
  * \param fmt <em>sprintf()</em>-style format string
+ * \param ... Additional arguments according to format string
+ *
+ * The default implementation of this function prints warnings messages to \a
+ * stderr, preceded by the string <tt>"pathcomp: "</tt>, and followed by a
+ * newline.
  */
 extern void pathcomp_log_warning(const char *fmt, ...);
 
 /**
  * Log a message at error level
  * \param fmt <em>sprintf()</em>-style format string
+ * \param ... Additional arguments according to format string
+ *
+ * The default implementation of this function prints error messages to \a
+ * stderr, preceded by the string <tt>"pathcomp: "</tt>, and followed by a
+ * newline.
  */
 extern void pathcomp_log_error(const char *fmt, ...);
 
