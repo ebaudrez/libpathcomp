3fd45ae574c4c870d509dbde9c0c79ded24369ee
Edward Baudrez <edward.baudrez@gmail.com>
2015-03-12 06:27:28 +0100

    test backslash continuation in config file

--- test/test_file.c
+++ test/test_file.c
@@ -32,6 +32,20 @@ test_file_and_string(void)
     pathcomp_free(c);
 }
 
+static void
+test_backslash(void)
+{
+    pathcomp_t *c = NULL;
+    char *s;
+
+    ok(c = pathcomp_new("test.backslash"));
+    is(s = pathcomp_eval(c, "value"), "some             value on several lines             which should be concatenated", "backslash continuation");
+    free(s);
+    is(s = pathcomp_eval(c, "other"), "something from whatever", "backslash continuation in Lua function");
+    free(s);
+    pathcomp_free(c);
+}
+
 int
 main(void)
 {
@@ -40,6 +54,7 @@ main(void)
     test_file();
     pathcomp_add_config_from_string("[test.file]\nto_be_created = ABC");
     test_file_and_string();
+    test_backslash();
     pathcomp_cleanup();
     done_testing();
 }
