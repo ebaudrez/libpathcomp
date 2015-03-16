e97df21e16e0d73394079b70fa1dde3cc2803763
Edward Baudrez <edward.baudrez@gmail.com>
2015-03-16 17:06:11 +0100

    test pathcomp_mkdir() with a pathname ending in a slash

--- test/test_mkdir.c
+++ test/test_mkdir.c
@@ -40,11 +40,40 @@ test_basic(void)
     pathcomp_free(c);
 }
 
+/* test pathname ending in a slash - is assumed to represent a directory and not a file */
+static void
+test_basic_dir_only(void)
+{
+    pathcomp_t *c = NULL;
+    char *s;
+
+    ok(c = pathcomp_new("test.mkdir"));
+    pathcomp_set(c, "compose", "lib/scratch/G2/SEV1/");
+    is(s = pathcomp_yield(c), "lib/scratch/G2/SEV1/");
+    free(s);
+    if (!dir_exists_ok("lib")) {
+        bail_out(0, "cannot find directory 'lib' - are you testing in subdirectory 'test'?");
+    }
+    path_not_exists_ok("lib/scratch");
+    path_not_exists_ok("lib/scratch/G2");
+    path_not_exists_ok("lib/scratch/G2/SEV1");
+    if (!ok(pathcomp_mkdir(c), "call pathcomp_mkdir()")) diag("pathcomp_mkdir: %s", strerror(errno));
+    dir_exists_ok("lib");
+    dir_exists_ok("lib/scratch");
+    dir_exists_ok("lib/scratch/G2");
+    dir_exists_ok("lib/scratch/G2/SEV1");
+    if (rmdir("lib/scratch/G2/SEV1") == -1) diag("rmdir: %s", strerror(errno));
+    if (rmdir("lib/scratch/G2") == -1) diag("rmdir: %s", strerror(errno));
+    if (rmdir("lib/scratch") == -1) diag("rmdir: %s", strerror(errno));
+    pathcomp_free(c);
+}
+
 int
 main(void)
 {
     plan(NO_PLAN);
     test_basic();
+    test_basic_dir_only();
     pathcomp_cleanup();
     done_testing();
 }
